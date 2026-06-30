758c1238dba7978a7a0b3ce644917bc4d622c122
devyansh vishwakarma <devyanshnayak@gmail.com>
2026-07-01 01:10:59 +0530

    added blog  and robot txt with seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.devyansh.site',
-	integrations: [sitemap()],
+	integrations: [
+    sitemap(),
+    robotsTxt(),
+  ],
 });
